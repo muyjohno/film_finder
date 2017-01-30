97d13f2a33120428dd222610318911303a7a8a22
John O'Brien <john.obrien@1minus1.com>
2017-01-30 21:58:25 +0000

    Add showings to interface

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,9 @@ class HomeController < ApplicationController
     lookup = CinemaService::Lookup.new(params[:postcode])
     @cinema = lookup.closest
   end
+
+  def showings
+    showings = CinemaService::Showings.new(params[:cinema_id])
+    @films = showings.get
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#show'
 
   get '/cinema', to: 'home#cinema'
+  get '/showings', to: 'home#showings'
 end
